b8e0d9bad216f99690d3fe9ae7027dc6c0b32eb9
TXP2023 <144341664+TXP2023@users.noreply.github.com>
2024-03-23 14:17:50 +0800

    Update 图-单源点最短路径-dijkstra.cpp

--- 图-单源点最短路径-dijkstra.cpp
+++ 图-单源点最短路径-dijkstra.cpp
@@ -1,7 +1,9 @@
+//原型题目 luogu P3371 AC
 //有权有向图
 
 #include <stdio.h>
 #include <vector>
+#include <math.h>
 #include <queue>
 #define ll long long
 
@@ -12,63 +14,75 @@ struct  anode
     ll k,right;//走到了节点k,累计权重为right
 };
 
-vector< vector< ll > > v;
+vector< vector< anode > > v;
 vector< ll > vis;
 queue< anode > nodes;
 ll N,M,K,L;//N个点 M条无相边 K次询问 从点L开始
 
 void BFS(ll l)
 {
-    anode Node;
+    anode Node;//建立一个类型为anode的结构体为队列的首位
     Node.k = l;
     Node.right = 0;
-    nodes.push(Node);
-    while (!nodes.empty())
+    nodes.push(Node);//压入队列
+    while (!nodes.empty())//如果队列不为空
     {
         anode node;
         node = nodes.front();
-        nodes.pop();
-        for (ll i = 0; i < N; i++)
+        nodes.pop();//弹出
+        for (ll i = 0; i < v[node.k].size(); i++)
         {
-            if(v[node.k][i]!=-1 && (vis[i] == -1 || vis[i] > v[node.k][i]+node.right) )
+            if((vis[ v[node.k][i].k ] == -1 || vis[v[node.k][i].k] > v[node.k][i].right+node.right) )//如果存在路径且
             {
-                vis[i] = v[node.k][i]+node.right;
+                vis[v[node.k][i].k] = v[node.k][i].right+node.right;
                 anode NODE;
-                NODE.k = i;
-                NODE.right = v[node.k][i]+node.right;
+                NODE.k = v[node.k][i].k;
+                NODE.right = v[node.k][i].right+node.right;
                 nodes.push(NODE);
             }
         }
     }
+    1;
     return;
 }
 
 int main()
 {
-    freopen("D:\\SHU_RU.in","r",stdin);
-    scanf("%lld%lld%lld%lld",&N,&M,&K,&L);
+    freopen("D:\\code\\C++\\SHU_RU.in","r",stdin);
+    scanf("%lld%lld%lld",&N,&M,&L);
     v.resize(N);
     vis.resize(N,-1);
-    for (ll i = 0; i < N; i++)
-    {
-        v[i].resize(N,-1);
-    }
     for (ll i = 0; i < M; i++)
     {
         ll from,to,right;
         scanf("%lld%lld%lld",&from,&to,&right);
-        if(from != to && (v[--from][--to]>right || v[from][to] == -1))
+        if(from != to )
         {
-            v[from][to] = right;
+            from--;
+            to--;
+            anode Anode;
+            Anode.k = to;
+            Anode.right = right;
+            v[from].push_back(Anode);
         }
     }
+    L--;
+    vis[L] = 0;
     BFS(L);
-    for (ll i = 0; i < K; i++)
+    for (ll i = 0; i < N; i++)
     {
-       ll x,y;
-       scanf("%lld",&x);
-       x--;
-       printf("%lld\n",vis[x]);
+        //printf("%lld%c",vis[i],' ');
+        switch (vis[i])
+        {
+        case -1:
+            printf("%lld%c",2147483647,' ');
+            break;
+        
+        default:
+            printf("%lld%c",vis[i],' ');
+            break;
+        }
     }
+    printf("\n");
     return 0;
 }
