b73127aafef345b1cbb69064b2a363e534ecfe67
TXP2023 <144341664+TXP2023@users.noreply.github.com>
2024-03-29 18:11:05 +0800

    Update 图-单源点最短路径-SPFA.cpp

--- 图-单源点最短路径-SPFA.cpp
+++ 图-单源点最短路径-SPFA.cpp
@@ -4,38 +4,30 @@
 #include <vector>
 #include <math.h>
 #include <queue>
-#define ll long
+#define ll long long
 
 using namespace std;
 
 struct  anode
 {
-    ll k,right;//走到了节点k,累计权重为right 987\57
-    bool operator <(const anode &a)const{
-		return right<a.right;
-	}
+    ll k,right;//走到了节点k,累计权重为right
 };
 
-priority_queue< anode > nodes;
 vector< vector< anode > > v;
 vector< ll > vis;
-ll N,M,L;//N个点 M条无相边 K次询问 从点L开始
+queue< anode > nodes;
+ll N,M,K,L;//N个点 M条无相边 K次询问 从点L开始
 
-inline void BFS(ll l)
-{        
+void BFS(ll l)
+{
     anode Node;//建立一个类型为anode的结构体为队列的首位
     Node.k = l;
     Node.right = 0;
     nodes.push(Node);//压入队列
     while (!nodes.empty())//如果队列不为空
     {
-        while(!nodes.empty() && vis[nodes.top().k] < nodes.top().right)
-            nodes.pop();
-
-        if(nodes.empty())
-            return;
-
-        anode node = nodes.top();
+        anode node;
+        node = nodes.front();
         nodes.pop();//弹出
         for (ll i = 0; i < v[node.k].size(); i++)
         {
@@ -49,21 +41,24 @@ inline void BFS(ll l)
             }
         }
     }
+    1;
     return;
 }
 
 int main()
 {
-    freopen("D:\\code\\C++\\SHU_RU.in","r",stdin);
-    scanf("%ld%ld%ld",&N,&M,&L);
+    //freopen("D:\\code\\C++\\SHU_RU.in","r",stdin);
+    scanf("%lld%lld%lld",&N,&M,&L);
     v.resize(N);
     vis.resize(N,-1);
     for (ll i = 0; i < M; i++)
     {
         ll from,to,right;
-        scanf("%ld%ld%ld",&from,&to,&right);
-        if(--from != --to )
+        scanf("%lld%lld%lld",&from,&to,&right);
+        if(from != to )
         {
+            from--;
+            to--;
             anode Anode;
             Anode.k = to;
             Anode.right = right;
@@ -75,14 +70,15 @@ int main()
     BFS(L);
     for (ll i = 0; i < N; i++)
     {
+        //printf("%lld%c",vis[i],' ');
         switch (vis[i])
         {
         case -1:
-            printf("%ld%c",-1,' ');
+            printf("%lld%c",2147483647,' ');
             break;
         
         default:
-            printf("%ld%c",vis[i],' ');
+            printf("%lld%c",vis[i],' ');
             break;
         }
     }
